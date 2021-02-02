7d88058e6df75da90fde34e2aa6c1f01386d05f8
bdrencher <bdrencher@gmail.com>
2021-02-01 22:13:52 -0500

    adding the location data structure

---
+++ Location.js
@@ -0,0 +1,12 @@
+/*********************************************************************************
+ * @description a class that contains the x and y location based on svg grid
+ ********************************************************************************/
+export class Location {
+    xPosition;
+    yPosition;
+
+    constructor(x, y) {
+        this.xPosition = x;
+        this.yPosition = y;
+    }
+}
