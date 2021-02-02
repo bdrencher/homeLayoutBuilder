1a55840c1f42782453b445ed622d976ec253495f
bdrencher <bdrencher@gmail.com>
2021-02-01 22:44:55 -0500

    changing Location's variables to match with what I've used in ShapeBuilder

--- Location.js
+++ Location.js
@@ -1,12 +1,12 @@
 /*********************************************************************************
  * @description a class that contains the x and y location based on svg grid
  ********************************************************************************/
-export class Location {
-    xPosition;
-    yPosition;
+class ShapeLocation {
+    x;
+    y;
 
     constructor(x, y) {
-        this.xPosition = x;
-        this.yPosition = y;
+        this.x = x;
+        this.y = y;
     }
 }
